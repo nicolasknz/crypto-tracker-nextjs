a95cabd11fce9727071c747fed562861257d0fb0
Nicolas <nicolasprodmac@gmail.com>
2021-05-09 02:45:41 -0300

    feat: added list of coins and individual page of specif ones

---
+++ components/CoinList/coin-list.jsx
@@ -0,0 +1,19 @@
+import Coin from "../Coin/coin";
+
+export default function CoinList({ filteredCoins }) {
+    return <>
+        {filteredCoins.map(coin => (
+            <Coin
+                key={coin.id}
+                name={coin.name}
+                id={coin.id}
+                price={coin.current_price}
+                symbol={coin.symbol}
+                marketcap={coin.market_cap}
+                volume={coin.total_volume}
+                image={coin.image}
+                priceChange={coin.price_change_percentage_24h}
+            />
+        ))}
+    </>;
+}
--- components/Coins/index.jsx
+++
@@ -1,9 +0,0 @@
-const Coins = () => {
-    return (
-        <div>
-            <h1>Coins</h1>
-        </div>
-    )
-}
-
-export default Coins
