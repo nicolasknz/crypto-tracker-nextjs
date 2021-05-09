97c156693c14b3681db400d7bf4ad1ef7594807e
Nicolas <nicolasprodmac@gmail.com>
2021-05-09 15:41:40 -0300

    feat: added static pages

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -4,13 +4,13 @@ import styles from './Coin.module.css'
 
 const Coin = ({coin}) => {
     return (
-        <Layout>
+        coin && <Layout>
             <div className={styles.coin_page}>
                 <div className={styles.coin_container}>
-                    <img className={styles.coin_image} src={coin.image.large}/>
+                    <img className={styles.coin_image} src={coin.image?.large}/>
                     <h1 className={styles.coin_name}></h1>
                     <p className={styles.coin_ticker}>{coin.symbol}</p>
-                    <p className={styles.coin_current}>{coin.market_data.current_price.usd}</p>
+                    <p className={styles.coin_current}>{coin.market_data?.current_price.usd}</p>
                 </div>
             </div>
         </Layout>
@@ -19,8 +19,27 @@ const Coin = ({coin}) => {
 
 export default Coin
 
-export async function getServerSideProps(context) {
-    const {id} = context.query
+export async function getStaticPaths() {
+    return {
+        paths: [
+            {
+                params: {
+                    id: "bitcoin"
+                }
+            },
+            {
+                params: {
+                    id: "ethereum"
+                }
+            }, 
+        ],
+        fallback: false
+    }
+}
+
+export async function getStaticProps(context) {
+    const {id} = context.params
+    
 
     const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
 
